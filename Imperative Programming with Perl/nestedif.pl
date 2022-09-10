b01303ce5cba6d265276219fe91a86417243420c
moaibad <fathulibad.412@gmail.com>
2022-09-10 17:03:51 +0700

    add nestedif.pl

---
+++ Imperative Programming with Perl/nestedif.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $x = 5.1;
+my $y = 5.1;
+
+if ( $x > 5.0 )
+{
+    if ( $y > 5.0 ){
+    print "x and y are greater than 5\n";
+    }
+}
+
+if ( ( $x > 5.0 ) and ( $y > 5.0 ) ){
+    print "x and y are greater than 5\n";
+}
