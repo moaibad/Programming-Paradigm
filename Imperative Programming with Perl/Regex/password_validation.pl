48d22016235350021630e2061bca2627857d303b
moaibad <fathulibad.412@gmail.com>
2022-09-19 17:01:00 +0700

    simplify the code

--- Imperative Programming with Perl/Regex/password_validation.pl
+++ Imperative Programming with Perl/Regex/password_validation.pl
@@ -4,13 +4,8 @@ use strict;
 my $input = <>;
 chomp $input;
 
-if ($input =~ m/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])\S{10}/){
-    if($input =~ m/\s/){   
-        print "Not Match\n";    
-    }
-    else{
-        print "Matches\n";
-    }
+if ($input =~ m/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.*[^-\s])\S{10}/){
+    print "Matches\n";
 }
 else{
     print "Not Match\n";
