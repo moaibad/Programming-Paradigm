6d78d8bb0c49a31a481d0a64c601170d049fe85f
moaibad <fathulibad.412@gmail.com>
2022-09-19 16:23:32 +0700

    update password_validation.pl

--- Imperative Programming with Perl/Regex/password_validation.pl
+++ Imperative Programming with Perl/Regex/password_validation.pl
@@ -1,3 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
 
+my $input = <>;
+chomp $input;
+
+if ($input =~ m/(?=.*[^a-z])(?=.*[^A-Z])(?=.*[^A-Za-z0-9])/){
+    print "Matches\n";
+}
+else{
+    print "Not Match\n";
+}
