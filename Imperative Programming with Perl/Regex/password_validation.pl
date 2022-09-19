97b9561e273f4d03bd0adf018b7477c4eff5fbba
moaibad <fathulibad.412@gmail.com>
2022-09-19 16:57:59 +0700

    add symbol, number, and minimum input validation

--- Imperative Programming with Perl/Regex/password_validation.pl
+++ Imperative Programming with Perl/Regex/password_validation.pl
@@ -4,8 +4,13 @@ use strict;
 my $input = <>;
 chomp $input;
 
-if ($input =~ m/^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9](?=.*[0-9]))/){
-    print "Matches\n";
+if ($input =~ m/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])\S{10}/){
+    if($input =~ m/\s/){   
+        print "Not Match\n";    
+    }
+    else{
+        print "Matches\n";
+    }
 }
 else{
     print "Not Match\n";
