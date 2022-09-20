04fae5ba2a6486696d93beb11431e0b20f315b9f
moaibad <fathulibad.412@gmail.com>
2022-09-20 09:14:11 +0700

    add for loop to validate the array

--- Imperative Programming with Perl/Regex/domain_web_validation.pl
+++ Imperative Programming with Perl/Regex/domain_web_validation.pl
@@ -1,15 +1,21 @@
 #!/usr/bin/perl -w
 use strict;
 
+
 my @domain = ("www.google.com","google.com","www.google.my","yahoo.com",
               "www.yahoo.com","www.abc123.id","www.abc123.sg",
-              "www.detik.net")
+              "www.detik.net");
 
-if ($domain1 =~ m/(www.)([a-z0-9]+)(.com|.id|.net)/){
-    print "$domain1 is Valid";
+
+for(my $i = 0; $i <= $#domain; $i++){
+
+    if ($domain[$i] =~ m/(www.)([a-z0-9]+)(.com|.id|.net)/){
+        print("$domain[$i] is Valid\n");
+    }
+    else{
+        print("$domain[$i] is Invalid\n");
+    }
 }
-else{
-    print "$domain1 is Invalid";
-}
+    
 
 
