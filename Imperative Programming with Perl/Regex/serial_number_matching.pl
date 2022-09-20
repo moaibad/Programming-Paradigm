d92b5551701f439f174e4a0038c0b0680d9e8cb1
moaibad <fathulibad.412@gmail.com>
2022-09-20 21:31:59 +0700

    update serial_number_matching.pl

--- Imperative Programming with Perl/Regex/serial_number_matching.pl
+++ Imperative Programming with Perl/Regex/serial_number_matching.pl
@@ -2,17 +2,18 @@
 
 use strict;
 
+print "Input Serial Number : ";
 my $input = <>;
 chomp $input;
 
 if ($input =~ m/^(\d{2})(\-)(\w{5})(\-)(\w{6})/){
     if(length($input) == 15){
-        print "$input is Valid Serial Number";
+        print "$input is valid serial number";
     }
     else{
-       print "$input is Invalid Serial Number"; 
+       print "$input is invalid serial number"; 
     }
 }
 else{
-    print "$input is Invalid Serial Number";
+    print "$input is invalid serial number";
 }
