acaafb6a8f87bf483fae0eb3041ba563e1abec78
moaibad <fathulibad.412@gmail.com>
2022-09-20 08:51:52 +0700

    put domain into single array

--- Imperative Programming with Perl/Regex/domain_web_validation.pl
+++ Imperative Programming with Perl/Regex/domain_web_validation.pl
@@ -1,14 +1,9 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $domain1 = "www.google.com";
-my $domain2 = "google.com";
-my $domain3 = "www.google.my";
-my $domain4 = "yahoo.com";
-my $domain5 = "www.yahoo.com";
-my $domain6 = "www.abc123.id";
-my $domain7 = "www.abc123.sg";
-my $domain8 = "www.detik.net";
+my @domain = ("www.google.com","google.com","www.google.my","yahoo.com",
+              "www.yahoo.com","www.abc123.id","www.abc123.sg",
+              "www.detik.net")
 
 if ($domain1 =~ m/(www.)([a-z0-9]+)(.com|.id|.net)/){
     print "$domain1 is Valid";
