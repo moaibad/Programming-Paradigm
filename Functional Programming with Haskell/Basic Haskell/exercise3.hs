dc2493d23ef9aa753ecdeeccf48eafdd1d02cf49
moaibad <fathulibad.412@gmail.com>
2022-10-03 22:29:44 +0700

    update exercise3.hs

---
+++ Functional Programming with Haskell/Basic Haskell/exercise3.hs
@@ -0,0 +1,12 @@
+checkEvenOddPosNeg :: Integer -> String
+checkEvenOddPosNeg (x) =
+    if x > 0 && x `mod` 2 == 0 then 
+        "Genap Positif"
+    else if x < 0 && x `mod` 2 == 0 then
+        "Genap Negatif"
+    else if x > 0 && x `mod` 2 == 1 then
+        "Ganjil Positif"
+    else if x < 0 && x `mod` 2 == 1 then
+        "Genap Negatif"
+    else
+        "Nol"
