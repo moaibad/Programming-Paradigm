f918645caa3c2c6d01a42f875fe77f9b40fc14f8
moaibad <fathulibad.412@gmail.com>
2022-10-06 21:17:54 +0700

    update exercise5.hs

---
+++ Functional Programming with Haskell/Basic Haskell/exercise5.hs
@@ -0,0 +1,18 @@
+comparePrime :: (Integer,Integer) -> Bool
+comparePrime (x,y) =
+    if y < 2 then
+        True 
+    else if x `mod` y == 0 && x /= y then
+        False
+    else
+        comparePrime(x,y-1)
+
+checkPrime :: Integer -> Bool
+checkPrime 1 = 
+    False
+checkPrime x =
+    comparePrime(x,x)
+
+primeList :: Integer -> [Integer]
+primeList x =
+    [y | y <- [2 .. x], checkPrime y]
