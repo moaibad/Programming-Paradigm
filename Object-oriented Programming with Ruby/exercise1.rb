58ec86bbb136c4c8845e2da9d7d7df17e9e179e2
moaibad <fathulibad.412@gmail.com>
2022-11-18 10:59:26 +0700

    update exercise1.rb

--- Object-oriented Programming with Ruby/exercise1.rb
+++ Object-oriented Programming with Ruby/exercise1.rb
@@ -11,9 +11,17 @@ class Vehicle
 end
 
 
+module OldtimerCheck
+    def isOldTimer?(year)
+        (2022-year) > 30
+    end
+end
+
+
 class Car < Vehicle
     attr_accessor :model
     attr_accessor :brand
+    include OldtimerCheck
 
     def initialize(year,model,brand)
         super(year)
@@ -25,11 +33,11 @@ class Car < Vehicle
         puts "#{model}"
         puts "#{brand}"
         super
+        puts isOldTimer?(year)
     end
+
 end
 
 
-mobil = Car.new("2022","Model","Yamaha")
+mobil = Car.new(1990,"Model","Yamaha")
 mobil.printStats()
-
-
