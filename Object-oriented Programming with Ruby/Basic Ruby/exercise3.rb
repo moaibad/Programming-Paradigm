674d28d0dc00a143dfb02304b2fd25451f2c9c04
moaibad <fathulibad.412@gmail.com>
2022-11-20 01:49:12 +0700

    update cost for mobil

--- Object-oriented Programming with Ruby/Basic Ruby/exercise3.rb
+++ Object-oriented Programming with Ruby/Basic Ruby/exercise3.rb
@@ -6,23 +6,22 @@ class Parkir
 end
 
 class ParkirMotor < Parkir
-    def initialize()
+    attr_accessor :nama
+    def initialize(nama)
         super(100)
+        self.nama = nama
     end
 
     def totalBiaya()
-        puts "Biaya Parkir Motor : $#{biaya_masuk}"
+        puts "Biaya Parkir Motor #{nama}: $#{biaya_masuk}"
     end
-
 end
 
 class ParkirMotorBiasa < ParkirMotor
-    attr_accessor :nama
     attr_accessor :lama
     attr_accessor :total_biaya
     def initialize(nama,lama)
-        super()
-        self.nama = nama
+        super(nama)
         self.lama = lama
     end
 
@@ -33,12 +32,10 @@ class ParkirMotorBiasa < ParkirMotor
 end
 
 class ParkirMotorBesar < ParkirMotor
-    attr_accessor :nama
     attr_accessor :lama
     attr_accessor :total_biaya
     def initialize(nama,lama)
-        super()
-        self.nama = nama
+        super(nama)
         self.lama = lama
     end
 
@@ -48,13 +45,65 @@ class ParkirMotorBesar < ParkirMotor
     end
 end
 
+class ParkirMobil < Parkir
+    attr_accessor :nama
+    def initialize(nama)
+        super(200)
+        self.nama = nama
+    end
+
+    def totalBiaya()
+        puts "Biaya Parkir Mobil #{nama}: $#{biaya_masuk}"
+    end
+end
+
+class ParkirMobilBiasa < ParkirMobil
+    attr_accessor :lama
+    attr_accessor :total_biaya
+    def initialize(nama,lama)
+        super(nama)
+        self.lama = lama
+    end
+
+    def totalBiaya()
+        total_biaya = (biaya_masuk)+((lama*60)*0.1)
+        puts "Biaya Parkir Mobil #{nama}: $#{total_biaya}"
+    end
+end
+
+class ParkirMobilSport < ParkirMobil
+    attr_accessor :lama
+    attr_accessor :total_biaya
+    def initialize(nama,lama)
+        super(nama)
+        self.lama = lama
+    end
+
+    def totalBiaya()
+        total_biaya = (biaya_masuk)+((lama*60)*10)
+        puts "Biaya Parkir Mobil #{nama}: $#{total_biaya}"
+    end
+end
 
 
+puts("+====================================+")
+puts("|         Biaya Parkir Mobil         |")
+puts("+====================================+")
+mobil1 = ParkirMobilSport.new("Ferrari",7)
+mobil1.totalBiaya()
 
-puts("==========Biaya Parkir Motor==========")
-motor = ParkirMotor.new()
-motor.totalBiaya()
+mobil2 = ParkirMobilSport.new("Lamborghini",10)
+mobil2.totalBiaya()
 
+mobil2 = ParkirMobilBiasa.new("Avanza",20)
+mobil2.totalBiaya()
+puts("======================================")
+
+puts("")
+
+puts("+====================================+")
+puts("|         Biaya Parkir Motor         |")
+puts("+====================================+")
 motor1 = ParkirMotorBesar.new("Harley Davidson",21)
 motor1.totalBiaya()
 
@@ -63,4 +112,4 @@ motor1.totalBiaya()
 
 motor1 = ParkirMotorBiasa.new("Vario",10)
 motor1.totalBiaya()
-
+puts("======================================")
